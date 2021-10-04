5b891dfb7360ea8c1f006cfd722fa7ac9aa3533e
Woozzang <woozzaang@gmail.com>
2021-10-05 01:32:00 +0900

    Project Setup

---
+++ MOMO/MOMO/Global/Custom/MomoBaseSearchField.swift
@@ -0,0 +1,12 @@
+//
+//  MomoBaseSearchField.swift
+//  MOMO
+//
+//  Created by Woochan Park on 2021/10/04.
+//
+
+import UIKit
+
+class MomoBaseSearchField: UITextField {
+
+}
---
+++ MOMO/MOMO/Global/Custom/MomoBaseTextField.swift
@@ -0,0 +1,12 @@
+//
+//  MoMoBaseTextField.swift
+//  MOMO
+//
+//  Created by Woochan Park on 2021/10/04.
+//
+
+import UIKit
+
+final class MomoBaseTextField: UITextField {
+
+}
