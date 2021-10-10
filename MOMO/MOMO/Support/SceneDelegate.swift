ac3bec1516ba13850e4899945a7a92e8d9dae2ba
pastapeter <pastapeter@naver.com>
2021-10-10 15:39:59 +0900

    feat: 로그인 여부에 따라 화면 분기

--- MOMO/MOMO/Support/SceneDelegate.swift
+++ MOMO/MOMO/Support/SceneDelegate.swift
@@ -9,14 +9,18 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+  var isLogged = false
   var window: UIWindow?
 
-
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-    guard let _ = (scene as? UIWindowScene) else { return }
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    window = UIWindow(windowScene: windowScene)
+    if !isLogged {
+       //로그인화면으로 ㄲ
+    } else {
+      //홈탭으로 ㄲ
+    }
+    self.window?.makeKeyAndVisible()
   }
 
   func sceneDidDisconnect(_ scene: UIScene) {
