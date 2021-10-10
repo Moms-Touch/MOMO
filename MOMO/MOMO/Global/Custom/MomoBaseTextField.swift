3527f77f845ff5acad9570509881146a83eb5f5a
pastapeter <pastapeter@naver.com>
2021-10-10 15:26:36 +0900

    feat: momobaseTextfield 구현

--- MOMO/MOMO/Global/Custom/MomoBaseTextField.swift
+++ MOMO/MOMO/Global/Custom/MomoBaseTextField.swift
@@ -8,5 +8,29 @@
 import UIKit
 
 final class MomoBaseTextField: UITextField {
-
+  private let figMaDefinedWidthValue: CGFloat = 295
+  private let figMaDefinedHeightValue: CGFloat = 47
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    applyMoMoDesign()
+  }
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    applyMoMoDesign()
+  }
+  
+  private func applyMoMoDesign() {
+    self.borderStyle = .none
+    self.layer.cornerRadius = self.frame.height / 2
+    self.layer.borderWidth = 1
+    self.heightAnchor.constraint(equalToConstant: figMaDefinedHeightValue).isActive = true
+    self.widthAnchor.constraint(equalToConstant: figMaDefinedWidthValue).isActive = true
+  }
+  
+  internal func setBorderColor(to color: UIColor) {
+    self.layer.borderColor = color.cgColor
+  }
+  
 }
