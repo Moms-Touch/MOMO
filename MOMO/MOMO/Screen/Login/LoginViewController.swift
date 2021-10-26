76e8d825e0543129b0f2d28914a1799dccb7dcf3
ohsg0226 <ohsg0226@naver.com>
2021-10-26 22:10:10 +0900

    feat: passwordTextField 추가

--- MOMO/MOMO/Screen/Login/LoginViewController.swift
+++ MOMO/MOMO/Screen/Login/LoginViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 import SnapKit
 
 class LoginViewController: UIViewController, StoryboardInstantiable {
-
+    
+    var idTextField = MomoBaseTextField(frame: CGRect())
+    var passwordTextField = MomoBaseTextField(frame: CGRect())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        addView()
         // Do any additional setup after loading the view.
+    }
+    
+    func addView() {
+        view.addSubview(idTextField)
+        view.addSubview(passwordTextField)
+        
+        idTextField.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(369)
+            make.leading.equalToSuperview().offset(39)
+        }
+        passwordTextField.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(idTextField).offset(66)
+            make.leading.equalTo(idTextField)
+        }
+        
+//
+//        NSLayoutConstraint.activate([idTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 39),
+//                                     idTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 369)])
     }
     
 
