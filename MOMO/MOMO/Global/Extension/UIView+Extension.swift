ec96361bba08f29d93d11e4dd020368736a1cb3d
pastapeter <pastapeter@naver.com>
2021-11-15 21:57:37 +0900

    feat: UIView+Extension - setRound 설정

--- MOMO/MOMO/Global/Extension/UIView+Extension.swift
+++ MOMO/MOMO/Global/Extension/UIView+Extension.swift
@@ -5,4 +5,17 @@
 //  Created by abc on 2021/11/13.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+  func setRound(_ radius: CGFloat? = nil) {
+    if let radius = radius {
+      self.layer.cornerRadius = radius
+    } else {
+      self.layer.cornerRadius = self.bounds.height / 2
+    }
+    self.layer.masksToBounds = true
+  }
+}
+
+
