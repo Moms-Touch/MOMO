a21487e973aea46cc682ae252702313e389f22cb
pastapeter <pastapeter@naver.com>
2021-11-15 21:58:14 +0900

    feat: UITextfield + Extension - AddLeftPadding 설정

--- MOMO/MOMO/Global/Extension/UITextfield+Extension.swift
+++ MOMO/MOMO/Global/Extension/UITextfield+Extension.swift
@@ -5,4 +5,12 @@
 //  Created by abc on 2021/11/14.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+  func addLeftPadding(width: CGFloat) {
+    let padding = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.bounds.height))
+    self.leftView = padding
+    self.leftViewMode = ViewMode.always
+  }
+}
