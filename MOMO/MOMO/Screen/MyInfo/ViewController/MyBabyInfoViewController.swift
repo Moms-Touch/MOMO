fe7b88485b3e61b0ac0460644ce07f701b940a27
pastapeter <pastapeter@naver.com>
2021-11-23 12:48:36 +0900

    feat: MyBabyInfo 추가

--- MOMO/MOMO/Screen/MyInfo/ViewController/MyBabyInfoViewController.swift
+++ MOMO/MOMO/Screen/MyInfo/ViewController/MyBabyInfoViewController.swift
@@ -7,23 +7,39 @@
 
 import UIKit
 
-class MyBabyInfoViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class MyBabyInfoViewController: UIViewController, StoryboardInstantiable {
+  
+  @IBOutlet weak var myBabyImageView: UIImageView!
+  @IBOutlet var myBabyInfoTextFields: [UITextField]! {
+    didSet {
+      for index in myBabyInfoTextFields.indices {
+        let textfield = myBabyInfoTextFields[index]
+        textfield.attributedPlaceholder = NSAttributedString(string: BabyEditTextfields.allCases[index].rawValue, attributes: [NSAttributedString.Key.foregroundColor: Asset.Colors.pink1, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18, weight: .medium)])
+        textfield.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        textfield.addLeftPadding(width: 20)
+        textfield.layer.borderColor = Asset.Colors.pink4.color.cgColor
+      }
     }
+  }
+  @IBOutlet weak var saveButton: UIButton! {
+    didSet {
+      saveButton.setRound(5)
+    }
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // Do any additional setup after loading the view.
+  }
+  
 }
+
+extension MyBabyInfoViewController {
+  enum BabyEditTextfields: String, CaseIterable {
+    case babyName = "아기이름"
+    case birth = "출생일/출생예정일"
+  }
+}
+
+extension MyBabyInfoViewController {}
