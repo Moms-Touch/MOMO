1e1a21b8169c4f9e64ea6032435045645e2c644f
pastapeter <pastapeter@naver.com>
2021-11-23 12:37:10 +0900

    feat: protocol CanShowDetailView - 디테일뷰로 넘어갈수 있게 만들어주는 protocol

---
+++ MOMO/MOMO/Global/Protocol/CanShowDetailView.swift
@@ -0,0 +1,28 @@
+//
+//  CanShowMyInfoVC.swift
+//  MOMO
+//
+//  Created by abc on 2021/11/22.
+//
+
+import UIKit
+
+// 하나의 detailView를 차용한다면, 이 protocol을 사용할 수 있다.
+// 아무래도 tableview vc, vc -> detail 
+
+protocol CanShowDetailView: AnyObject {
+  func showDetailView(content: Any)
+  var navigationController: UINavigationController {get}
+    // Any는 나중에 Model로 변경한다.
+    // Model은 특정 protocol을 받아야할 것이다.
+}
+
+extension CanShowDetailView {
+  func showDetailView(content: Any) {
+    let detailViewController = DetailViewController()
+    detailViewController.content = content
+    self.navigationController.pushViewController(detailViewController, animated: true)
+  }
+}
+
+
