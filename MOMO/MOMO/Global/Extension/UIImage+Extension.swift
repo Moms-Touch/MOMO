629dc311a3a1d1e486cf7b18b7fd1b1fb33c1ae6
pastapeter <pastapeter@naver.com>
2021-11-23 12:21:27 +0900

    feat: UIImage + Extension 추가 - Color로 이미지 만드는 코드

---
+++ MOMO/MOMO/Global/Extension/UIImage+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIImage+Extension.swift
+//  MOMO
+//
+//  Created by abc on 2021/11/22.
+//
+
+import UIKit
+
+extension UIImage {
+  class func colorImage(color: CGColor, size: CGSize) -> UIImage{
+    UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+    let graphicsContext = UIGraphicsGetCurrentContext()
+    graphicsContext?.setFillColor(color)
+    graphicsContext?.fill(CGRect(x: 0.0, y: 0.0, width: size.width, height: size.height))
+    let colorImage = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return colorImage!
+  }
+}
