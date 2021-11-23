9b78d974b697f6fb58022bbff0e31e6dcd851299
pastapeter <pastapeter@naver.com>
2021-11-23 12:53:23 +0900

    feat: DeviceManager로 필요한 경우 기기대응

---
+++ MOMO/MOMO/Support/DeviceGroup.swift
@@ -0,0 +1,14 @@
+//
+//  DeviceGroup.swift
+//  MOMO
+//
+//  Created by abc on 2021/11/22.
+//
+
+import Foundation
+
+public enum DeviceGroup {
+  case fourInches
+  case fiveInches
+  case XSeries
+}
---
+++ MOMO/MOMO/Support/DeviceManager.swift
@@ -0,0 +1,15 @@
+//
+//  DeviceManager.swift
+//  MOMO
+//
+//  Created by abc on 2021/11/22.
+//
+
+import Foundation
+
+class DeviceManager {
+  static let shared = DeviceManager()
+  
+  private init() {}
+  
+}
