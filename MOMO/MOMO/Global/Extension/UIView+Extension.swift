39da0114fc87141d30fb2b44fb0b6bfb692c83f4
pastapeter <pastapeter@naver.com>
2021-11-23 12:23:34 +0900

    feat: UIView + Extension : Border를 4분면에 따로 줄수 있는 코드

--- MOMO/MOMO/Global/Extension/UIView+Extension.swift
+++ MOMO/MOMO/Global/Extension/UIView+Extension.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 extension UIView {
+  
+  // 모서리 둥글게
   func setRound(_ radius: CGFloat? = nil) {
     if let radius = radius {
       self.layer.cornerRadius = radius
@@ -15,6 +17,27 @@ extension UIView {
       self.layer.cornerRadius = self.bounds.height / 2
     }
     self.layer.masksToBounds = true
+  }
+  
+  // 4방면에 border 주는 것
+  func addBorder(_ arrEdge: [UIRectEdge], color: UIColor, width: CGFloat){
+    for edge in arrEdge {
+      let border = CALayer()
+      switch edge {
+      case UIRectEdge.top:
+        border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: width)
+      case UIRectEdge.bottom:
+        border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+      case UIRectEdge.left:
+        border.frame = CGRect.init(x: 0, y: 0, width: width, height: frame.height)
+      case UIRectEdge.right:
+        border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+      default:
+        break
+      }
+      border.backgroundColor = color.cgColor
+      self.layer.addSublayer(border)
+    }
   }
 }
 
