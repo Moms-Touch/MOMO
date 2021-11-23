e704cc5cac2855af3e5c6daa004487b89f4cebed
pastapeter <pastapeter@naver.com>
2021-11-23 12:50:54 +0900

    feat: HomeMainVC 생성 및 추가 + PanModel 추가

--- MOMO/MOMO/Screen/Home/ViewController/HomeMainViewController.swift
+++ MOMO/MOMO/Screen/Home/ViewController/HomeMainViewController.swift
@@ -8,37 +8,81 @@
 import UIKit
 import PanModal
 
-class HomeMainViewController: UIViewController, StoryboardInstantiable {
+final class HomeMainViewController: UIViewController, StoryboardInstantiable {
   
   @IBOutlet weak var noticeView: UIView! {
     didSet {
-      noticeView.backgroundColor = UIColor(rgb: 0x000000).withAlphaComponent(0.1)
+      noticeView.backgroundColor = .black.withAlphaComponent(0.1)
     }
   }
+  
+  @IBOutlet weak var imageHuggingView: UIView! {
+    didSet {
+      imageHuggingView.setRound()
+    }
+  }
+  @IBOutlet weak var babyProfileImageView: UIImageView! {
+    didSet {
+      babyProfileImageView.setRound()
+      babyProfileImageView.isUserInteractionEnabled = true
+      let tapgesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gotoMyProfile(_:)))
+      babyProfileImageView.addGestureRecognizer(tapgesture)
+      //Usermodel을 observing 하고 있어야함
+    }
+  }
+  @IBOutlet weak var dateWithBabyButton: UIButton! {
+    didSet {
+      dateWithBabyButton.setRound()
+      dateWithBabyButton.backgroundColor = .white
+      //Usermodel을 observing 하고 있어야함
+    }
+  }
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     assignbackground()
   }
   
-  func assignbackground(){
-         let background = UIImage(named: "mainBackground")
-
-         var imageView : UIImageView!
-         imageView = UIImageView(frame: view.bounds)
-         imageView.contentMode =  UIView.ContentMode.scaleAspectFill
-         imageView.clipsToBounds = true
-         imageView.image = background
-         imageView.center = view.center
-         view.addSubview(imageView)
-         self.view.sendSubviewToBack(imageView)
-     }
+  private func assignbackground(){
+    let background = UIImage(named: "mainBackground")
+    
+    var imageView : UIImageView!
+    imageView = UIImageView(frame: view.bounds)
+    imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+    imageView.clipsToBounds = true
+    imageView.image = background
+    imageView.center = view.center
+    view.addSubview(imageView)
+    self.view.sendSubviewToBack(imageView)
+  }
   
-  @IBAction func didTapRecommendButton(_ sender: UIButton) {
+  @IBAction private func didTapRecommendButton(_ sender: UIButton) {
     presentPanModal(RecommendModalViewController())
   }
-  @IBAction func didTapTodayButton(_ sender: UIButton) {
+  
+  @IBAction private func didTapTodayButton(_ sender: UIButton) {
     print(#function)
   }
+  
+  @IBAction private func didTapBookmarkListButton(_ sender: UIButton) {
+    self.navigationController?.pushViewController(BookmarkListViewController.loadFromStoryboard(), animated: true)
+  }
+  
+  @IBAction private func didTapProfile(_ sender: Any) {
+    gotoMyProfile()
+  }
+  
+  @objc private func gotoMyProfile(_ sender: Any?) {
+    gotoMyProfile()
+  }
+  
+  private func gotoMyProfile() {
+    self.navigationController?.pushViewController(MyInfoMainViewController.loadFromStoryboard(), animated: true)
+  }
+  
+  
   
 }
 
--- MOMO/MOMO/Screen/Home/ViewController/RecommendModalViewController.swift
+++ MOMO/MOMO/Screen/Home/ViewController/RecommendModalViewController.swift
@@ -6,24 +6,40 @@
 //
 
 import UIKit
+import PanModal
 
 class RecommendModalViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+    // Do any additional setup after loading the view.
+  }
+  
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension RecommendModalViewController: PanModalPresentable {
+  var panScrollable: UIScrollView? {
+    return nil
+  }
+  
+  var shortFormHeight: PanModalHeight {
+    return .contentHeight(200)
+  }
+  
+  var longFormHeight: PanModalHeight {
+    return .maxHeightWithTopInset(400)
+  }
+  
+  var panModalBackgroundColor: UIColor {
+    return .black.withAlphaComponent(0.2)
+  }
+  
+  var showDragIndicator: Bool {
+    return true
+  }
+  
+  var isUserInteractionEnabled: Bool {
+    return true
+  }
 }
