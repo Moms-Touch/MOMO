e8defea34b8b73450725813202a7b129f38eac9b
pastapeter <pastapeter@naver.com>
2021-11-23 12:53:37 +0900

    feat: DetailVC 추가

---
+++ MOMO/MOMO/Screen/Policy/DetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DetailViewController.swift
+//  MOMO
+//
+//  Created by abc on 2021/11/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+  
+  var content: Any?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
