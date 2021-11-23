06ffd3ccd718819209f59b70be47c4a835841382
pastapeter <pastapeter@naver.com>
2021-11-23 12:48:19 +0900

    feat: MyInfoEditVC 생성

--- MOMO/MOMO/Screen/MyInfo/ViewController/MyInfoEditViewController.swift
+++ MOMO/MOMO/Screen/MyInfo/ViewController/MyInfoEditViewController.swift
@@ -7,23 +7,59 @@
 
 import UIKit
 
-class MyInfoEditViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class MyInfoEditViewController: UIViewController, StoryboardInstantiable {
+  
+  @IBOutlet weak var myProfileImageView: UIImageView!
+  
+  @IBOutlet weak var saveButton: UIButton! {
+    didSet {
+      saveButton.setRound(5)
     }
+  }
+  @IBOutlet weak var infoEditScrollView: UIScrollView!
+  
+  @IBOutlet var myInfoTextFields: [UITextField]! {
+    didSet {
+      for index in myInfoTextFields.indices {
+        let textfield = myInfoTextFields[index]
+        textfield.attributedPlaceholder = NSAttributedString(string: EditTextfields.allCases[index].rawValue, attributes: [NSAttributedString.Key.foregroundColor: Asset.Colors.pink4.color, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 18, weight: .medium)])
+        textfield.font = UIFont.systemFont(ofSize: 18, weight: .medium)
+        textfield.addLeftPadding(width: 20)
+        textfield.layer.borderColor = Asset.Colors.pink4.color.cgColor
+      }
+    }
+  }
+  
+  @IBOutlet weak var distFromSaveButton: NSLayoutConstraint!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    myProfileImageView.setRound()
+    scrollingKeyboard()
+  }
+  
+  @IBAction func didTapSaveButton(_ sender: UIButton) {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
 }
+
+extension MyInfoEditViewController {
+  enum EditTextfields: String, CaseIterable {
+    case nick = "닉네임"
+    case email = "이메일 계정"
+    case cellPhone = "핸드폰 번호"
+  }
+  
+  
+}
+
+extension MyInfoEditViewController: KeyboardScrollable {
+  
+  var scrollView: UIScrollView {
+    return infoEditScrollView
+  }
+  
+}
+
