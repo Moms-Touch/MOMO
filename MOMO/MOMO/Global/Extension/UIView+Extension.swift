086125ee2ae655bba42eedbe193268222b2bcb0f
pastapeter <pastapeter@naver.com>
2021-11-28 11:20:53 +0900

    feat: UIView+ Extension에 꼭지점 각각에 round 주는것

--- MOMO/MOMO/Global/Extension/UIView+Extension.swift
+++ MOMO/MOMO/Global/Extension/UIView+Extension.swift
@@ -39,6 +39,14 @@ extension UIView {
       self.layer.addSublayer(border)
     }
   }
+  
+  func setRound(_ corners: UIRectCorner, radius: CGFloat) {
+    let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+    let mask = CAShapeLayer()
+    mask.path = path.cgPath
+    layer.mask = mask
+  }
+  
 }
 
 
