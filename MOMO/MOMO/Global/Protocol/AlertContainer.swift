acdf632a6e510c8682f502ec384cba725a5ff3b1
pastapeter <pastapeter@naver.com>
2021-12-01 00:02:38 +0900

    feat: 알림과 쪽찌를 위한 protocol 후에 작성 예정

---
+++ MOMO/MOMO/Global/Protocol/AlertContainer.swift
@@ -0,0 +1,16 @@
+//
+//  alertContainer.swift
+//  MOMO
+//
+//  Created by abc on 2021/11/29.
+//
+
+import Foundation
+import UIKit
+
+protocol AlertContainer { }
+
+extension AlertContainer where Self: UITableViewCell {
+  
+}
+
