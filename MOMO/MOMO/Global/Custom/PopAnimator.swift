2ec8893421b3047c98c300a34cf4c0f665c1334f
pastapeter <pastapeter@naver.com>
2021-12-17 08:31:48 +0900

    feat: RecommendModalVC에 pop Animation 구현

--- MOMO/MOMO/Global/Custom/PopAnimator.swift
+++ MOMO/MOMO/Global/Custom/PopAnimator.swift
@@ -7,60 +7,60 @@
 
 import UIKit
 
-fileprivate let transitionDurationTime: TimeInterval = 1.0
-
-enum PresentingType {
-  case present
-  case dismiss
-}
-
-class PopAnimator: NSObject {
-  let presentingType: PresentingType
+class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
   
-  init(presentingType: PresentingType) {
-    self.presentingType = presentingType
-    super.init()
-  }
-}
-
-extension PopAnimator: UIViewControllerAnimatedTransitioning {
+  private let duration = 0.3
+  var presenting = true
+  var originFrame = CGRect.zero
+  
+  var dismissCompletion: (()->Void)?
+  
   func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-    return transitionDurationTime
+    return duration
   }
   
   func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
     
-  }
-}
-
-extension PopAnimator {
-  func animationForPresent(using transitionContext: UIViewControllerContextTransitioning) {
-    
     let containerView = transitionContext.containerView
+    let recommendCell = presenting ? transitionContext.view(forKey: .to)!: transitionContext.view(forKey: .from)!
     
-    guard let fromVC = transitionContext.viewController(forKey: .from) as? UITabBarController else {return}
-    guard let toVC = transitionContext.viewController(forKey: .to) as? SettingWebViewController else {return}
+    let initialFrame = presenting ? originFrame : recommendCell.frame
+    let finalFrame = presenting ? recommendCell.frame : originFrame
     
-//    guard let selectedCell = fromVC.selectedCell else {return}
+    let xScaleFactor = presenting ?
+      initialFrame.width / finalFrame.width : finalFrame.width / initialFrame.width
     
-//    let frame = selectedCell.convert(selectedCell.thumbNailImageView.frame, to: fromVC.view)
+    let yScaleFactor = presenting ?
+      initialFrame.height / finalFrame.height : finalFrame.height / initialFrame.height
     
-//    toVC.view.frame = frame
-    toVC.view.layer.cornerRadius = 10
+    let scaleTransform = CGAffineTransform(scaleX: xScaleFactor, y: yScaleFactor)
     
-    containerView.addSubview(toVC.view)
+    if presenting {
+      recommendCell.transform = scaleTransform
+      recommendCell.center = CGPoint(x: initialFrame.midX, y: initialFrame.midY)
+      recommendCell.clipsToBounds = true
+    }
+    
+    if let toView = transitionContext.view(forKey: .to) {
+      containerView.addSubview(toView)
+    }
+    containerView.bringSubviewToFront(recommendCell)
+    
+    UIView.animate(withDuration: duration) {
+      recommendCell.transform = self.presenting ? CGAffineTransform.identity : scaleTransform
+      print(finalFrame)
+      print(initialFrame)
+      print("finalFrame.midx, midy", finalFrame.midX, finalFrame.midY)
+      print("initical.midx, midy", initialFrame.midX, initialFrame.midY)
+      recommendCell.center = CGPoint(x: finalFrame.midX, y: finalFrame.midY)
+    } completion: { _ in
+      if !self.presenting {
+        self.dismissCompletion?()
+      }
+      transitionContext.completeTransition(true)
+    }
+    
   }
   
-  func animationForDismiss(using transitionContext: UIViewControllerContextTransitioning) {
-    guard let fromVC = transitionContext.viewController(forKey: .from) as? SettingWebViewController else {return}
-    guard let toVC = transitionContext.viewController(forKey: .to) as? TabBar else {return}
-//    guard let selectedCell = mainVC.
-    
-    UIView.animate(withDuration: transitionDurationTime / 2, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: [], animations: {
-      
-    }) {_ in
-      
-    }
-  }
   
 }
