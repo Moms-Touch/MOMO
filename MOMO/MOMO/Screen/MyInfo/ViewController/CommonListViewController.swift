3c9b734f8e237df67fde13da6553390d3f0dc0d6
pastapeter <pastapeter@naver.com>
2021-12-30 19:21:02 +0900

    feat: CommonListVC를 추가, 현재는 좋아요만 쓰이지만 추후에 계속 쓰일 예정

---
+++ MOMO/MOMO/Screen/MyInfo/ViewController/CommonListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CommonListViewController.swift
+//  MOMO
+//
+//  Created by abc on 2021/12/30.
+//
+
+import UIKit
+
+class CommonListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
