72e56e7b2b6e752f34e42d22bdf03c96050b832e
pastapeter <pastapeter@naver.com>
2021-12-30 19:22:23 +0900

    feat: 좋아요 리스트를 빼기 위한 api, VC

--- MOMO/MOMO/Screen/MyInfo/ViewController/CommonListViewController.swift
+++ MOMO/MOMO/Screen/MyInfo/ViewController/CommonListViewController.swift
@@ -7,23 +7,54 @@
 
 import UIKit
 
-class CommonListViewController: UIViewController {
+class CommonListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, TableViewGappable {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  var headerHeight: CGFloat {
+    return gapBWTCell
+  }
+  
+  @IBOutlet weak var titleLabel: UILabel!
+  
+  var footerHeight: CGFloat {
+    return gapBWTCell
+  }
+  @IBOutlet weak var tableview: UITableView!
 
-        // Do any additional setup after loading the view.
-    }
+  private var gapBWTCell:CGFloat = 10;
+  var datasource: [SimpleCommunityData] = []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    tableview.dataSource = self
+    tableview.delegate = self
+    tableview.register(ListTableViewCell.self)
+  }
+  
+  @IBAction func didtapBackButton(_ sender: UIButton) {
+    self.navigationController?.popViewController(animated: true)
+  }
+}
+
+extension CommonListViewController: StoryboardInstantiable {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return datasource.count
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.identifier, for: indexPath) as! ListTableViewCell
+    cell.getSimpleData(data: datasource[indexPath.section])
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
+  func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    scrollView.bounces = scrollView.contentOffset.y > 0
+  }
 }
