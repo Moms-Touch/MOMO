1a27ff3b356d09573ae22ba93a4766cbbf5315b3
pastapeter <pastapeter@naver.com>
2022-01-04 21:24:20 +0900

    fix: 좋아요 목록 삭제

--- MOMO/MOMO/Screen/MyInfo/ViewController/MyActivityViewController.swift
+++ MOMO/MOMO/Screen/MyInfo/ViewController/MyActivityViewController.swift
@@ -33,28 +33,7 @@ class MyActivityTableViewController: InfoBaseTableViewController {
   
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     switch indexPath.row {
-    case 0: //LikeList
-      guard let token = UserManager.shared.token else {return}
-      networkManager.request(apiModel: GetApi.likeGet(token: token)) { (result) in
-        switch result {
-        case .success(let data):
-          let parsingManager = ParsingManager()
-          parsingManager.judgeGenericResponse(data: data, model: [LikeData].self) { [weak self] (body) in
-            guard let self = self else {return}
-            let simpleCommunityData = body.compactMap { $0.community }
-            let datasource = simpleCommunityData.filter {$0.isValid == true && $0.isDeleted == false }
-            DispatchQueue.main.async {
-              guard let vc = CommonListViewController.loadFromStoryboard() as? CommonListViewController else {return}
-              vc.datasource = datasource
-              print(datasource)
-              self.navigationController?.pushViewController(vc, animated: true)
-            }
-          }
-        case .failure(let error):
-          print(error)
-        }
-      }
-    case 1:
+    case 0:
       let vc = BookmarkListViewController.loadFromStoryboard() as! BookmarkListViewController
       vc.mode = false
       self.navigationController?.pushViewController(vc, animated: true)
@@ -64,3 +43,25 @@ class MyActivityTableViewController: InfoBaseTableViewController {
   }
   
 }
+
+// 좋아요
+//guard let token = UserManager.shared.token else {return}
+//networkManager.request(apiModel: GetApi.likeGet(token: token)) { (result) in
+//  switch result {
+//  case .success(let data):
+//    let parsingManager = ParsingManager()
+//    parsingManager.judgeGenericResponse(data: data, model: [LikeData].self) { [weak self] (body) in
+//      guard let self = self else {return}
+//      let simpleCommunityData = body.compactMap { $0.community }
+//      let datasource = simpleCommunityData.filter {$0.isValid == true && $0.isDeleted == false }
+//      DispatchQueue.main.async {
+//        guard let vc = CommonListViewController.loadFromStoryboard() as? CommonListViewController else {return}
+//        vc.datasource = datasource
+//        print(datasource)
+//        self.navigationController?.pushViewController(vc, animated: true)
+//      }
+//    }
+//  case .failure(let error):
+//    print(error)
+//  }
+//}
