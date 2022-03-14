21664f5cd3b58bd8399ecd3725553fb7ab5e6c92
pastapeter <pastapeter@naver.com>
2022-03-14 18:04:14 +0900

    feat: MomoUserRemoteAPI 완료

--- MOMO/MOMO/Global/DataLayer/Repositories/Remote/UserRemoteAPI/Implementation/MomoUserRemoteAPI.swift
+++ MOMO/MOMO/Global/DataLayer/Repositories/Remote/UserRemoteAPI/Implementation/MomoUserRemoteAPI.swift
@@ -13,33 +13,55 @@ final class MomoUserRemoteAPI: UserRemoteAPI {
   //MARK: - Private Properties
   private let networkManager: NetworkProtocol
   private let decoder: NetworkDecoding
+  private let userSession: UserSession
   
   //MARK: - init
-  public init(networkManager: NetworkProtocol, decoder: NetworkDecoding) {
+  public init(networkManager: NetworkProtocol, decoder: NetworkDecoding, userSession: UserSession) {
     self.networkManager = networkManager
     self.decoder = decoder
-  }
-
-  //MARK: - Methods
-
-  func readUserSession() -> Observable<UserSession> {
-    
+    self.userSession = userSession
   }
   
+  //MARK: - Methods
+  
+  func readUserSession() -> Observable<UserSession> {
+    let token = userSession.token
+    return networkManager.request(apiModel: GetApi.userGet(token: token))
+      .asObservable()
+      .flatMap { [weak self] data -> Observable<UserSession> in
+        guard let self = self else { return Observable.error(CodingError.decodingError)}
+        
+        let observable = self.decoder.decode(data: data, model: UserData.self)
+          .map {
+            return UserSession(profile: $0, token: token)
+          }
+        
+        return observable
+      }
+  }
   
   func deleteUser() -> Completable {
-    <#code#>
+    let token = userSession.token
+    return networkManager.request(apiModel: DeleteApi.deleteUser(token: token))
+      .asCompletable()
   }
   
   func updateUserInfo(with info: UserData) -> Observable<UserData> {
-    <#code#>
+    let token = userSession.token
+    return networkManager.request(apiModel: PutApi.putUser(token: token, email: info.email , nickname: info.nickname, isPregnant: info.isPregnant, hasChild: info.hasChild, age: info.age, location: info.location))
+      .asObservable()
+      .flatMap { [weak self] data -> Observable<UserData> in
+        guard let self = self else {return Observable.error(CodingError.decodingError)}
+        return self.decoder.decode(data: data, model: UserData.self)
+      }
   }
   
+  // cloud API not yet
+  @available(*, deprecated)
   func updatePassword(from old: String, with new: String) -> Completable {
-    <#code#>
+    return Completable.create { complete in
+      complete(.completed) as! Disposable
+    }
   }
-  
-  
 
-  
 }
