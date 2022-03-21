5c6beafaf7fb99e4d50b81a10ed3d9424b9c5753
pastapeter <pastapeter@naver.com>
2022-03-21 21:17:45 +0900

    feat: MomoRecommendRemoteAPI 기능 구현 및 폴더 구조 변경

---
+++ MOMO/MOMO/Global/DataLayer/Remote/RecommendRemoteAPI/MomoRecommendRemoteAPI.swift
@@ -0,0 +1,63 @@
+//
+//  MomoRecommendRemoteAPI.swift
+//  MOMO
+//
+//  Created by abc on 2022/03/20.
+//
+
+import Foundation
+import RxSwift
+
+final class MomoRecommendRemoteAPI: RecommendRemoteAPI {
+  
+  //MARK: - Private Properties
+
+  private let networkManager: NetworkProtocol
+  private let decoder: NetworkDecoding
+  
+  //MARK: - init
+  init(networkManager: NetworkProtocol, decoder: NetworkDecoding) {
+    self.networkManager = networkManager
+    self.decoder = decoder
+  }
+  
+  //MARK: - Methods
+  
+  func fetch(token: String, start: Int, end: Int) -> Observable<[InfoData]> {
+    return networkManager.request(apiModel: GetApi.infoGet(token: token, start: "\(start)", end: "\(end)"))
+      .asObservable()
+      .withUnretained(self)
+      .flatMap { remoteAPI, data in
+        return remoteAPI.decoder.decode(data: data, model: [InfoData].self)
+      }
+      .share()
+  }
+  
+  func fetch(token: String, id: Int) -> Observable<InfoData> {
+    return networkManager.request(apiModel: GetApi.infoDetailGet(token: token, id: id))
+      .asObservable()
+      .withUnretained(self)
+      .flatMap { remoteAPI, data in
+        return remoteAPI.decoder.decode(data: data, model: InfoData.self)
+      }
+      .share()
+  }
+  
+  func bookmark(token: String, id: Int, category: Category) -> Completable {
+    return networkManager.request(apiModel: PostApi.postBookmark(token: token, postId: id, postCategory: category))
+      
+      .withUnretained(self)
+      .flatMap { remoteAPI, data -> Observable<Bool> in
+        return Observable.just(true)
+      }
+  }
+  
+  func unbookmark(token: String, id: Int, category: Category) -> Observable<Bool> {
+    return networkManager.request(apiModel: DeleteApi.deleteBookmark(token: token, postId: id, postCategory: category))
+      .asObservable()
+      .flatMap { data -> Observable<Bool> in
+        return Observable.just
+      }
+  }
+
+}
