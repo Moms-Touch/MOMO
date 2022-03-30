9fb1cfc7cb9c89146ed213306b6e26540fe20717
pastapeter <pastapeter@naver.com>
2022-03-31 03:17:57 +0900

    feat: UIViewController + Extension에 alert 추가

--- MOMO/MOMO/Global/Extension/UIViewController+Extension.swift
+++ MOMO/MOMO/Global/Extension/UIViewController+Extension.swift
@@ -22,4 +22,31 @@ extension UIViewController {
       }
     }
   }
+  
+  func alertWithOneAnswer(title: String, text: String?, answer: String) -> Completable {
+    return Completable.create { [weak self] completable in
+      let alertVC = UIAlertController(title: title, message: text, preferredStyle: .alert)
+      alertVC.addAction(UIAlertAction(title: answer, style: .default, handler: { _ in
+        completable(.completed)
+      }))
+      self?.present(alertVC, animated: true, completion: nil)
+      return Disposables.create {
+        self?.dismiss(animated: true, completion: nil)
+      }
+    }
+  }
+  
+  func alertWithObservable(title: String, text: String?) -> Observable<Bool> {
+    return Observable.create { observer in
+      let alertVC = UIAlertController(title: title, message: text, preferredStyle: .alert)
+      alertVC.addAction(UIAlertAction.cancelAction)
+      alertVC.addAction(UIAlertAction(title: "네", style: .default, handler: { _ in
+        observer.onNext(true)
+      }))
+     self.present(alertVC, animated: true, completion: nil)
+      return Disposables.create()
+    }
+  }
+  
+  
 }
