5090748baf25cfd0f29facda3ea364d3f38c7cc7
pastapeter <pastapeter@naver.com>
2022-04-04 23:03:06 +0900

    refactor: 안에 컨테이너 vc의 viewmodel add

--- MOMO/MOMO/UILayer/Diary/CreateDiary/ViewModel/WithInputViewModel.swift
+++ MOMO/MOMO/UILayer/Diary/CreateDiary/ViewModel/WithInputViewModel.swift
@@ -6,11 +6,19 @@
 //
 
 import Foundation
+import OrderedCollections
 
 // WithTextViewModel, WithVoiceViewModel의 부모viewmodel
 
 class WithInputViewModel {
   private var hasGuide: Bool
+  
+  var qnaList: OrderedDictionary<String, String> = [:]
+  var defaultQuestion = "자유롭게 일기를 작성해주세요."
+  var guideQuestionList: [String] = [ "오늘 하루는 어떠셨나요?",
+                                      "오늘 아이에게 무슨 일이 있었나요?",
+                                      "남기고 싶은 말이 있나요?"
+  ]
   
   init(hasGuide: Bool) {
     self.hasGuide = hasGuide
