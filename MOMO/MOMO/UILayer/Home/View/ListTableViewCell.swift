1f751cd2f30493d4889dc4b35b273f841c819a8a
pastapeter <pastapeter@naver.com>
2022-04-07 19:26:38 +0900

    refactor: 서버 업데이트로 인한 썸네일 추가

--- MOMO/MOMO/UILayer/Home/View/ListTableViewCell.swift
+++ MOMO/MOMO/UILayer/Home/View/ListTableViewCell.swift
@@ -41,41 +41,6 @@ class ListTableViewCell: UITableViewCell, NibLoadableView, simpleContentContaine
       
       dateLabel.text = (data?.createdAt ?? "").trimStringDate()
       
-      if let author = data?.author, author.contains("여성가족부") == true {
-        thumbnailImageView.image = UIImage(named: "governmentLogo")!
-        
-        return
-      }
-      
-      if let author = data?.author, author.contains("인트리") == true {
-        thumbnailImageView.image = UIImage(named: "intree")!
-        
-        return
-      }
-      
-      if let author = data?.author, author.contains("한국미혼모지원네트워크") == true {
-        thumbnailImageView.image = UIImage(named: "koreaSinglemomNetwork")!
-        
-        return
-      }
-      
-      if let author = data?.author, author.contains("러브더월드") == true {
-        thumbnailImageView.image = UIImage(named: "loveTheWorld")!
-        
-        return
-      }
-      
-      if let author = data?.author, author.contains("동감") == true {
-        thumbnailImageView.image = UIImage(named: "dongkam")!
-        
-        return
-      }
-      
-      if let author = data?.author, author.contains("한국미혼모가족협회") == true {
-        thumbnailImageView.image = UIImage(named: "singlemomAssoication")!
-        
-        return
-      }
       guard let thumbnailImageUrl = data?.thumbnailImageUrl else {
         thumbnailImageView.image = UIImage(named: "Logo")!
         return
